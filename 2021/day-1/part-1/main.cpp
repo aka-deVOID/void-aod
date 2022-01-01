619baae950875fb41225e37eb3bf07312f07960d
MahanBi <DirtyMBGamer@gmail.com>
2022-01-01 16:03:04 +0330

    Day1 Part1 C++

--- 2021/day-1/part-1/main.cpp
+++ 2021/day-1/part-1/main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <sstream>
-
-void to_array(std::ifstream file, int array_one) {
-
-}
+#include <vector>
 
 int main(){
     std::ifstream FILE("inputs.txt");
     
     int count = 0;
-    std::string line;
-
-    while (std::getline(FILE, line)) {
-        count++;
-        std::cout << line;
-        int copy_count = count;
-        {
-            std::string new_line;
-            FILE.seekg(copy_count--);
-            std::getline(FILE, new_line);
+    int plug = -1;
+    
+    for (std::string line; std::getline(FILE, line);) {
+        int x = std::stoi(line);
+        if (plug != -1 && x > plug) {
+            count++;
         }
-        //if () {
-
-        //}
+        plug = x;
     }
-
-
-
+    std::cout << count;
     return 0;
 }
