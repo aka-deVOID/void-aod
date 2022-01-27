786be93d6cb4caabb5302b655e3d8029c25e797b
MahanBi <DirtyMBGamer@gmail.com>
2022-01-28 02:04:54 +0330

    add: main.jl

---
+++ 2021/day-1/part-1/main.jl
@@ -0,0 +1,12 @@
+@time function main()::Int
+    filedata = open("inputs.txt", "r");
+    data = readlines(filedata);
+    count = 0;
+    for (index, line) in enumerate(data)
+        if parse(Int, line) > parse(Int, index == 1 ? line : data[index-1])
+            count += 1;
+        end
+    end
+    return count
+end
+println(main())
---
+++ 2021/day-2/part-1/main.py
@@ -0,0 +1,13 @@
+def main(file):
+    omgh = 0
+    location = 0
+    for line in file:
+        
+
+
+
+
+
+if __name__ == "__main__":
+    file = open("inputs.txt")
+    print(main(file.readlines()))
