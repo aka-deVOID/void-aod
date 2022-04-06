e1c853292a267fe8a1fcf4921ada90da2c34aee5
MahanBi <DirtyMBGamer@gmail.com>
2022-04-06 12:45:46 -0700

    Complete: day-3

--- 2021/day-3/part-1/main.jl
+++ 2021/day-3/part-1/main.jl
@@ -1,9 +1,11 @@
-function main()
-    data = readlines(open("inputs.txt", "r"))
-    gama, epsilon = "", ""
-    for byte in data
-        for bit in byte
-            ...
-        end
-    end
-end
+# Code From https://github.com/DearRude/advent-of-code/blob/main/2021/day-03/part-1.jl
+input_file = open("inputs.txt", "r")
+binaries = split.(readlines(input_file), " ") .|> x -> x[1]
+
+gamma_bits =
+    [count(==('0'), binaries .|> x -> x[idx]) > length(binaries) / 2 ? '0' : '1'
+    for (idx, _) ∈ binaries[1] |> enumerate]
+
+pad = 64 - length(gamma_bits)
+gamma_rate = parse(UInt, gamma_bits |> join, base=2)
+@info gamma_rate * (~gamma_rate << pad >> pad)
---
+++ 2021/day-3/part-2/main.jl
@@ -0,0 +1,17 @@
+# Code From https://github.com/DearRude/advent-of-code/blob/main/2021/day-03/part-2.jl
+input_file = open("inputs.txt", "r")
+binaries = split.(readlines(input_file), " ") .|> x -> x[1]
+
+function filter_occur(bins, idx, least)
+    more_zero = count(==('0'), bins .|> x -> x[idx]) > length(bins) / 2
+    char_filter = (!more_zero) ⊻ least ? '1' : '0'
+    filter(x -> x[idx] == char_filter, bins)
+end
+
+mosts = leasts = binaries
+for (idx, _) ∈ binaries[1] |> enumerate
+    length(mosts) > 1 && (global mosts = filter_occur(mosts, idx, false))
+    length(leasts) > 1 && (global leasts = filter_occur(leasts, idx, true))
+end
+
+@info parse(Int, mosts[1], base=2) * parse(Int, leasts[1], base=2)
