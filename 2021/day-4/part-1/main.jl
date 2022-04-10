5728758442dea9a2f13a92f1a658a1b8a27866e2
MahanBi <DirtyMBGamer@gmail.com>
2022-04-10 02:24:03 -0700

    Write: Find Nums

--- 2021/day-4/part-1/main.jl
+++ 2021/day-4/part-1/main.jl
@@ -1,11 +1,23 @@
 datas = readlines(open("inputs.txt", "r"))
 first_line = parse.(Int, split(popfirst!(datas), ",")); pushfirst!(datas)
 board = Vector{Matrix{Int}}(undef, length(datas) ÷ 6)
-#println(datas)
 for (i, j) in enumerate(1:6:length(datas)-1)
     board[i] = Matrix{Int}(undef, 5, 5)
     for line_index in 1:5
         board[i][line_index, :] .= parse.(Int, split(datas[j+line_index]))
     end
 end
-println(board)
+new = []
+for find_this in first_line
+    for (index, matrix) in enumerate(board)
+        println(index)
+        for col in eachcol(matrix)
+            for (index, item) in enumerate(col)
+            
+            end
+        end
+        for row in eachrow(matrix)
+            println(row)
+        end
+    end
+#println(board)
