d9bbe95a247961364d2822459c37415a67a67d31
MahanBi <DirtyMBGamer@gmail.com>
2022-04-12 14:40:52 -0700

    day-6 part-1 TODO

---
+++ 2021/day-6/part-1/main.jl
@@ -0,0 +1,9 @@
+data = parse.(Int, split(readline(open("inputs.txt")), ","))
+print(data)
+function per_day(data)
+
+    changed = data
+for i in 1:80
+    return length(data)
+end
+println(per_day(data))
