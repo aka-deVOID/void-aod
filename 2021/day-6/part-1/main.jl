37408b88f5c00eb6485f2690ac9c6b187f887348
MahanBi <DirtyMBGamer@gmail.com>
2022-04-13 05:13:51 -0700

    Complete: day-6

--- 2021/day-6/part-1/main.jl
+++ 2021/day-6/part-1/main.jl
@@ -1,9 +1,19 @@
 data = parse.(Int, split(readline(open("inputs.txt")), ","))
-print(data)
-function per_day(data)
 
-    changed = data
-for i in 1:80
-    return length(data)
+function per_day(data, days)
+
+    sim = Dict(i => count(==(i), data) for i in 0:8)
+    idx = vcat(0:5, 7)
+
+    for _ in 1:days
+        old = copy(sim)
+        for i in idx
+            sim[i] = old[i+1]
+        end
+        sim[6] = old[7] + old[0]
+        sim[8] = old[0]
+    end
+    return sum(values(sim))
+
 end
-println(per_day(data))
+println(per_day(data, 80))
