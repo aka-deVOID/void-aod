96619cdce3d77c46dfb583bb5121e035f3c825c5
MahanB <dirtymbgamer@gmail.com>
2022-12-18 22:36:04 +0330

    2022 - 2 - 1

---
+++ 2022/day-2/part-1/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::read_to_string;
+
+fn main() {
+    // A: ROCK, B: PAPER, C: Scissors // X: ROCK +1 , Y: PAPER +2, Z: Scissors +3 // Lose: 0 , mosavi: 3 , Win: 6
+    let file_data = read_to_string("input.txt")
+        .expect("Critical Error!!")
+        .split('\n')
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    
+    
+    let mut a;
+    let mut point: u32 = 0;
+
+    for line in &file_data {
+        a = line
+            .split(" ")
+            .collect::<String>()
+            .chars()
+            .collect::<Vec<char>>();
+        
+        if a[1] == 'X' {
+            point += 1;
+            if a[0] == 'A' {
+                point += 3;
+            } else if a[0] == 'C' {
+                point += 6;
+            };
+        } else if a[1] == 'Y' {
+            point += 2;
+            if a[0] == 'A' {
+                point += 6;
+            } else if a[0] == 'B' {
+                point +=3
+            };
+        } else if a[1] == 'Z' {
+            point += 3;
+            if a[0] == 'B' {
+                point += 6;
+            } else if a[0] == 'C' {
+                point += 3;
+            };
+        };
+    };
+    println!("{}", point)
+}
+
